5a316f5fd218f19e1cab8761fbb337066994ad7c
Marek Bečvář <markbecvar@gmail.com>
2021-07-27 01:48:30 +0200

    names changes

---
+++ grainSim/GrainSim/Elements/Water.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace GrainSim
+{
+    class Water : Element
+    {
+        public Water()
+        {
+            this.ID = ElementID.WATER;
+
+            this.name = "Water";      
+            this.nameShort = "WATER";
+            this.description = $"{name}: Pure liquid";
+            this.color = Color.DeepSkyBlue;
+
+            this.state = 1;           
+            this.weight = 1;         
+            this.move = true;
+            this.spawnTemperature = 20;
+
+            this.flameable = 0;
+            this.explosive = 0;   
+
+            this.heatTransfer = 0.6f;
+
+            this.lowLevelTemp = 0;
+            this.lowLevelTempTransition = new Reaction(this.ID,
+                                                       new List<ElementID>() {ElementID.ICE},
+                                                       0.5f); 
+            this.highLevelTemp = 100;
+            this.highLevelTempTransition = new Reaction(this.ID,
+                                                        new List<ElementID>() {ElementID.WATERVAPOR},
+                                                        0.5f); 
+
+            this.reactions.Add(new Reaction(this.ID,
+                                            new List<ElementID>() {ElementID.SALTWATER},
+                                            ElementID.SALT,
+                                            1,
+                                            0.03f,
+                                            true));
+
+            DefaultReactions(this);
+        }
+    }
+}
